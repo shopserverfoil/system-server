9e0253d85af02c056c3b59c5c94955a13c1738f2
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-09-27 23:01:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
@@ -10,7 +10,6 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
-
 
 
 var ss = 0;
