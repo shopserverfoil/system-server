90ed2dae573b46eecad837390a772ce913cec664
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-09-27 21:39:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+
+
 client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('489800301021233163');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
