543435459c8bc94f00aff686ca8111e4a8fb9619
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-09-28 22:41:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,7 +45,7 @@ client.on("ready", () => {
 
 
 client.on('message', message => {
-    var prefix = ""
+    var prefix = "-"
   if (message.author.x5bz) return;
   if (!message.content.startsWith(prefix)) return;
 
@@ -54,7 +54,7 @@ client.on('message', message => {
 
   let args = message.content.split(" ").slice(1);
 
-  if (command == "كيك") {
+  if (command == "kick") {
                if(!message.channel.guild) return message.reply('** This command only for servers**');
          
   if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
