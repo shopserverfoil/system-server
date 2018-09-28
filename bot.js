d502ff58376bd5f5e1a2cf8b44d4161fdc59a988
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-09-28 23:47:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,61 +44,61 @@ client.on("ready", () => {
 
 
 
-client.on('message', ALPHACODES => { 
-  var sender = ALPHACODES.author
-  if(!ALPHACODES.guild) return
-  if(!sw[ALPHACODES.guild.id]) sw[ALPHACODES.guild.id] = {
+client.on('message', MEGA => { 
+  var sender = MEGA.author
+  if(!MEGA.guild) return
+  if(!sw[MEGA.guild.id]) sw[MEGA.guild.id] = {
   onoff: 'Off',
   ch:    'Welcome',
   msk:   'Welcome'
 }
-        if(ALPHACODES.content.startsWith(prefix + `set-wlc`)) {        
-        let perms = ALPHACODES.member.hasPermission(`MANAGE_CHANNELS`)
-        if(!perms) return ALPHACODES.channel.send('**You need `Manage Channels` permission**')
-        let args = ALPHACODES.content.split(" ").slice(1)
-        if(!args.join(" ")) return ALPHACODES.reply(`
+        if(MEGA.content.startsWith(prefix + `set-wlc`)) {        
+        let perms = MEGA.member.hasPermission(`MANAGE_CHANNELS`)
+        if(!perms) return MEGA.channel.send('**You need `Manage Channels` permission**')
+        let args = MEGA.content.split(" ").slice(1)
+        if(!args.join(" ")) return MEGA.reply(`
   ** ${prefix}set-wlc toggle **
   ** ${prefix}set-wlc set [Channel Name] **
-  ** ${prefix}set-wlc msg [Welcome ALPHACODES] **`) // ->set-wlc toggle - ->set-wlc set - ->set-wlc msg
+  ** ${prefix}set-wlc msg [Welcome MEGA] **`) // ->set-wlc toggle - ->set-wlc set - ->set-wlc msg
         let state = args[0]
-        if(!state.trim().toLowerCase() == 'toggle' || !state.trim().toLowerCase() == 'set' || !state.trim().toLowerCase() == 'msg' ) return ALPHACODES.reply(`
+        if(!state.trim().toLowerCase() == 'toggle' || !state.trim().toLowerCase() == 'set' || !state.trim().toLowerCase() == 'msg' ) return MEGA.reply(`
  ** ${prefix}set-wlc toggle **
  ** ${prefix}set-wlc set [Channel Name] **
- ** ${prefix}set-wlc msg [Welcome ALPHACODES] **`) // ->set-wlc toggle - ->set-wlc set - ->set-wlc msg
+ ** ${prefix}set-wlc msg [Welcome MEGA] **`) // ->set-wlc toggle - ->set-wlc set - ->set-wlc msg
         if(state.trim().toLowerCase() == 'toggle') { 
-        if(sw[ALPHACODES.guild.id].onoff === 'Off') return [ALPHACODES.channel.send(`**Welcome ALPHACODES Is **on** !**`), sw[ALPHACODES.guild.id].onoff = 'On']
-        if(sw[ALPHACODES.guild.id].onoff === 'On')  return [ALPHACODES.channel.send(`**Welcome ALPHACODES Is **off** !**`), sw[ALPHACODES.guild.id].onoff = 'Off']
+        if(sw[MEGA.guild.id].onoff === 'Off') return [MEGA.channel.send(`**Welcome MEGA Is **on** !**`), sw[MEGA.guild.id].onoff = 'On']
+        if(sw[MEGA.guild.id].onoff === 'On')  return [MEGA.channel.send(`**Welcome MEGA Is **off** !**`), sw[MEGA.guild.id].onoff = 'Off']
 }
         if(state.trim().toLowerCase() == 'set') {
-        let newch = ALPHACODES.content.split(" ").slice(2).join(" ")
-        if(!newch) return ALPHACODES.reply(`${prefix}set-wlc set [Channel name]`)
-        if(!ALPHACODES.guild.channels.find(`name`,newch)) return ALPHACODES.reply(`**I Cant Find This Channel.**`)
-            sw[ALPHACODES.guild.id].ch = newch
-            ALPHACODES.channel.send(`**Welcome channel Has Been Changed to ${newch}.**`)
+        let newch = MEGA.content.split(" ").slice(2).join(" ")
+        if(!newch) return MEGA.reply(`${prefix}set-wlc set [Channel name]`)
+        if(!MEGA.guild.channels.find(`name`,newch)) return MEGA.reply(`**I Cant Find This Channel.**`)
+            sw[MEGA.guild.id].ch = newch
+            MEGA.channel.send(`**Welcome channel Has Been Changed to ${newch}.**`)
 } 
         if(state.trim().toLowerCase() == 'msg') {
-        let newmsg = ALPHACODES.content.split(" ").slice(2).join(" ")
-        if(!newmsg) return ALPHACODES.reply(`${prefix}set-wlc msg [New ALPHACODES]`)
-            sw[ALPHACODES.guild.id].msk = newmsg
-            ALPHACODES.channel.send(`**Welcome ALPHACODES Has Been Changed to ${newmsg}.**`)
+        let newmsg = MEGA.content.split(" ").slice(2).join(" ")
+        if(!newmsg) return MEGA.reply(`${prefix}set-wlc msg [New MEGA]`)
+            sw[MEGA.guild.id].msk = newmsg
+            MEGA.channel.send(`**Welcome MEGA Has Been Changed to ${newmsg}.**`)
 } 
 }
-        if(ALPHACODES.content === prefix + 'set-wlc info') {
-        let perms = ALPHACODES.member.hasPermission(`MANAGE_GUILD`) 
-        if(!perms) return ALPHACODES.reply(`You don't have permissions.`)
+        if(MEGA.content === prefix + 'set-wlc info') {
+        let perms = MEGA.member.hasPermission(`MANAGE_GUILD`) 
+        if(!perms) return MEGA.reply(`You don't have permissions.`)
         var embed = new Discord.RichEmbed()
-        .addField(`Welcome ALPHACODES  `, `
-On/Off  : __${sw[ALPHACODES.guild.id].onoff}__
-Channel : __${sw[ALPHACODES.guild.id].ch}__
-ALPHACODES : __${sw[ALPHACODES.guild.id].msk}__`)
+        .addField(`Welcome MEGA  `, `
+On/Off  : __${sw[MEGA.guild.id].onoff}__
+Channel : __${sw[MEGA.guild.id].ch}__
+MEGA : __${sw[MEGA.guild.id].msk}__`)
         .setColor(`BLUE`)
-        ALPHACODES.channel.send({embed})
+            MEGA.channel.send({embed})
 }
         fs.writeFile("./setwlc.json", JSON.stringify(sw), (err) => {
         if (err) console.error(err)
 });
 })
-//by ALPHA CODES 
+//by MEGA
 
 
 
