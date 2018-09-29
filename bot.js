cb4e83d6cd4b6882e5c92c07240ce7feef64c020
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-09-29 16:08:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '!';
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+
 
 
 var ss = 0;
