82b4752505b3d6902f1b026191d9bca36f1e7f15
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-09-29 13:51:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,9 +68,9 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-    var prefix = "-";
+    var prefix = "";
    if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'clear')) {
+if(message.content.startsWith(prefix + 'مسح')) {
 if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
 if(!message.member.hasPermission('MANAGE_MESSAGES')) return      message.channel.send('**You Do not have permission** `MANAGE_MESSAGES`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
