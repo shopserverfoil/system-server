a9fe3432208abd293f89d7852160d09b60c2b2bc
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-09-30 22:56:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,29 +44,6 @@ message.react("❌")
 
 
 
-client.on('message', message => {
-   
-    let args = message.content.split(' ').slice(1).join(' ');
-   
-  if (message.content === 'ping') {
-      message.channel.send(`<@${message.author.id}> Ping..!`)
-  }
- 
- 
-  if (message.content.startsWith('رسالة')) 
-          if (!args[0]) {
-message.channel.send("**يجب عليك كتابة }رسالة{ واكتب رسالتك هنا  :envelope:  **");
-return;
-}
-message.guild.members.forEach(m => {
-   if(!message.member.hasPermission('ADMINISTRATOR')) return;
-   m.send(`${args}`);
- 
-});
-  }
- 
-});
-
 
 
 
