0c55f3e689118d35eb58ae5aaf6e6694898910aa
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-09-30 23:00:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,4 +29,37 @@ client.user.setGame(` اكتب اي شي هنا  `,"http://twitch.tv/S-F")
 
 
 
+
+
+
+client.on('message', message => {
+   
+    let args = message.content.split(' ').slice(1).join(' ');
+   
+  if (message.content === 'ping') {
+      message.channel.send(`<@${message.author.id}> Ping..!`)
+  }
+ 
+ 
+  if (message.content.startsWith('رسالة')) 
+          if (!args[0]) {
+message.channel.send("**يجب عليك كتابة }رسالة{ واكتب رسالتك هنا  :envelope:  **");
+return;
+}
+message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+   m.send(`${args}`);
+ 
+});
+  }
+ 
+});
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
