87bed91083be1e2c5d499e0ff41b17d4befced25
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-09-30 22:58:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,54 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!';
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` اكتب اي شي هنا  `,"http://twitch.tv/S-F")
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online ')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
 });
-
-
-
-
-client.on('message', message => {
-    var prefix = "";
-if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'اسحب')) {
- if (message.member.hasPermission("MOVE_MEMBERS")) {
- if (message.mentions.users.size === 0) {
- return message.channel.send("" +prefix+ "** يجب أن تمنشن العضو  ❌**")
-}
-if (message.member.voiceChannel != null) {
- if (message.mentions.members.first().voiceChannel != null) {
- var authorchannel = message.member.voiceChannelID;
- var usermentioned = message.mentions.members.first().id;
-var embed = new Discord.RichEmbed()
- .setTitle("Succes!")
- 
- 
-var embed = new Discord.RichEmbed()
-
- 
-.setDescription(`**<@${message.author.id}> لقد تم سحب العضو إليك ✅**`)
- message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
-message.guild.members.get(usermentioned).send(embed)
-} else {
-message.channel.send(""+ message.mentions.members.first() +"**❌ أن العضو ليس بروم صوتي**")
-}
-} else {
- message.channel.send("**أنت لست متواجد في روم صوتي لسحب العضو إليك ❌**")
-}
-} else {
-message.react("❌")
- }}});
-
-
-
-
-
-
-
-
-
 
 
 
