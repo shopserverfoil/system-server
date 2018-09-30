1d3e213c89886ca886729bd995ee9095c414225d
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-01 00:22:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` اكتب اي شي هنا  `,"http://twitch.tv/S-F")
+client.user.setGame(`system server`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -258,6 +258,71 @@ client.on('message', async message => { // Alpha Codes Server.
             })
         }
     }
+});
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    var prefix = "";
+if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'اسحب')) {
+ if (message.member.hasPermission("MOVE_MEMBERS")) {
+ if (message.mentions.users.size === 0) {
+ return message.channel.send("" +prefix+ "** يجب أن تمنشن العضو  ❌**")
+}
+if (message.member.voiceChannel != null) {
+ if (message.mentions.members.first().voiceChannel != null) {
+ var authorchannel = message.member.voiceChannelID;
+ var usermentioned = message.mentions.members.first().id;
+var embed = new Discord.RichEmbed()
+ .setTitle("Succes!")
+ 
+ 
+var embed = new Discord.RichEmbed()
+
+ 
+.setDescription(`**<@${message.author.id}>لقد تم سحب العضو إليك ✅**`)
+ message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
+message.guild.members.get(usermentioned).send(embed)
+} else {
+message.channel.send(""+ message.mentions.members.first() +"**❌ أن العضو ليس بروم صوتي**")
+}
+} else {
+ message.channel.send("**أنت لست متواجد في روم صوتي لسحب العضو إليك ❌**")
+}
+} else {
+message.react("❌")
+ }}});
+
+
+
+
+
+
+
+client.on('message', function(msg) {
+  if(msg.content.startsWith ('$server')) {
+    if(!msg.channel.guild) return msg.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
+    let embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setThumbnail(msg.guild.iconURL)
+    .addField(':globe_with_meridians: **اسم السيرفر : **' , `**[ ${msg.guild.name} ]**`,true)
+    .addField(':earth_africa: ** موقع السيرفر :**',`**[ ${"Egypt"} ]**`,true)
+    .addField(':military_medal:** الرتب :**',`**[ ${msg.guild.roles.size} ]**`,true)
+    .addField(':bust_in_silhouette:** عدد الاعضاء :**',`**[ ${msg.guild.memberCount} ]**`,true)
+    .addField(':white_check_mark:** عدد الاعضاء الاونلاين :**',`**[ ${msg.guild.members.filter(m=>m.presence.status == 'online').size} ]**`,true)
+    .addField(':pencil:** الرومات الكتابية :**',`**[ ${msg.guild.channels.filter(m => m.type === 'text').size} ]**`,true)
+    .addField(':loud_sound:** رومات الصوت :**',`**[ ${msg.guild.channels.filter(m => m.type === 'voice').size} ]**`,true)
+    .addField(':crown:** صاحب السيرفر :**',`**[ ${msg.guild.owner} ]**`,true)
+    .addField(':id:** ايدي السيرفر :**',`**[ ${msg.guild.id} ]**`,true)
+    .addField(':date:** تم عمل السيرفر في : **',msg.guild.createdAt.toLocaleString())
+    msg.channel.send({embed:embed});
+  }
 });
 
 
