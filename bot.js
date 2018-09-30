745a1eda094a5baad903f7e920449ec220e638fb
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-09-30 22:30:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,30 +9,6 @@ client.on('ready', () => {
 
 
 
-client.on('message', message => {
-     if (message.author.bot) return;
-    if (message.content.startsWith("رابط")) {
-        message.channel.createInvite({
-        thing: true,
-        maxUses: 5,
-        maxAge: 3600,
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-       
-          .setDescription("**:mailbox_with_mail: تم أرسال الرابط برسالة خاصة**")
-           .setAuthor(client.user.username, client.user.avatarURL)
-                 .setAuthor(client.user.username, client.user.avatarURL)
-                .setFooter('طلب بواسطة: ' + message.author.tag)
-
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        
-    .setDescription(" مدة الرابط : ساعة  عدد استخدامات الرابط : 5 ")
-      message.author.sendEmbed(Embed11)
-    }
 
 
 
