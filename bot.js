26180916d81136da6ee82856307eccbf4924d378
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-01 00:43:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '!';
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online ')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+    console.log(`Logged in as ${client.user.tag}`);
 });
+
+
+
 
 
 var ss = 0;
@@ -55,6 +38,9 @@ client.on("ready", () => {
     client6.user.setGame("HerpoPlex Community©", "https://twitch.tv/©");
 });
 // جميع الحقوق محفوظة لدى : ! M, ♪ ♡#6456
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
