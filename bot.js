8797301bf567bbac8b6984799a8e7ced87a15a2a
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-01 19:55:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -153,35 +153,7 @@ message.channel.send(""+ message.mentions.members.first() +"**❌ أن العض�
 }
 } else {
  message.channel.send("")
- }}});
 
-
-
-
-
-
-
-client.on('message', message => {
-    if (message.content.startsWith("-avatar")) {
-        if (message.author.bot) return
-        var mentionned = message.mentions.users.first();
-    var omar;
-      if(mentionned){
-          var omar = mentionned;
-      } else {
-          var omar = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setAuthor('Avatar Link :')
-        .setTitle('Click Here')
-        .setURL(`${omar.avatarURL}`)
-        .setImage(`${omar.avatarURL}`)
-        .setFooter('name bot',client.user.avatarURL) 
-      message.channel.sendEmbed(embed);
-    }
-});
 
 
 
