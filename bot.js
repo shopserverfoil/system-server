e060d10d46b60331b9538e19c4b7c6b28d722e7e
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-01 18:40:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -80,7 +80,7 @@ client.on('message', message => {
 	if(command == prefix + 'رسالة') { // الكوماند !bc
 		var args = message.content.split(' ').slice(1).join(' ');
 		if(message.author.bot) return;
-		if(!args) return message.channel.send(`** :incoming_envelope: { :incoming_envelope: رسالة { كلامك ** ${prefix} `);
+		if(!args) return message.channel.send(`** { :incoming_envelope: رسالة { كلامك ** ${prefix} `);
 		
 		let bcSure = new Discord.RichEmbed()
 		.setTitle(`:mailbox_with_mail: **هل انت متأكد انك تريد ارسال رسالتك الى** ${message.guild.memberCount} **عضو**`)
