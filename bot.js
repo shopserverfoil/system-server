fe53fbe382df5730c64940bee6738dc60e766f82
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-01 23:13:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -219,11 +219,17 @@ client.on('message', message => {
 
     if (!actionlog) return message.channel.send('');
     const embedlog = new Discord.RichEmbed()
-      .setDescription('**:wastebasket: تم المسح **')
+      .setDescription('**            :wastebasket: تم المسح **')
       .setColor("RED")
-      .addField('**:arrow_down:  تم المسح بواسطة **', `<@${message.author.id}> with ID ${message.author.id}`)
-      .addField('**:arrow_down:  المسؤل عن المسح**', message.channel)
-      .addField('**:arrow_down:  تم المسح في تاريخ**', message.createdAt);
+      
+    
+    .addField('**:arrow_down:  تم المسح بواسطة **', `<@${message.author.id}> with ID ${message.author.id}`)
+      
+    
+    .addField('**:arrow_down:  المسؤل عن المسح**', message.channel)
+      
+    
+    .addField('**:arrow_down:  تم المسح في تاريخ**', message.createdAt);
     actionlog.send(embedlog);
    
   });
