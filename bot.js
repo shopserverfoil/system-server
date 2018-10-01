8283a8ac852f21ae1d93f7e8662980a9b9c5e887
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-01 19:47:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -158,6 +158,37 @@ message.channel.send(""+ message.mentions.members.first() +"**❌ أن العض�
 
 
 
+client.on('message', message => {
+     if (message.author.bot) return;
+    if (message.content.startsWith("رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 5,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription(" تم أرسال الرابط برسالة خاصة ")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription(" مدة الرابط : ساعة : عدد استخدامات الرابط : 5 ")
+      message.author.sendEmbed(Embed11)
+    }
+
+
+
+
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
