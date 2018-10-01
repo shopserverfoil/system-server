178dcd21664067fc51e4eeb0ff17a3b218d3179c
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-01 19:37:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -122,4 +122,42 @@ client.on('message', message => {
 
 
 
+
+client.on('message', message => {
+    var prefix = "";
+if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'اسحب')) {
+ if (message.member.hasPermission("MOVE_MEMBERS")) {
+ if (message.mentions.users.size === 0) {
+ return message.channel.send("" +prefix+ "** يجب أن تمنشن العضو  ❌**")
+}
+if (message.member.voiceChannel != null) {
+ if (message.mentions.members.first().voiceChannel != null) {
+ var authorchannel = message.member.voiceChannelID;
+ var usermentioned = message.mentions.members.first().id;
+var embed = new Discord.RichEmbed()
+ .setTitle("Succes!")
+ 
+ 
+var embed = new Discord.RichEmbed()
+
+ 
+.setDescription(`**<@${message.author.id}>لقد تم سحب العضو إليك ✅**`)
+ message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
+message.guild.members.get(usermentioned).send(embed)
+} else {
+message.channel.send(""+ message.mentions.members.first() +"**❌ أن العضو ليس بروم صوتي**")
+}
+} else {
+ message.channel.send("**أنت لست متواجد في روم صوتي لسحب العضو إليك ❌**")
+}
+} else {
+ message.channel.send("")
+ }}});
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
