2a73bc736e513bebd373f17897fbcc3231b1c86d
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-01 23:51:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -320,18 +320,6 @@ const embed = new Discord.RichEmbed()
 
 
 
-var anti_spam = require("discord-anti-spam");
- 
-antispam(bot, {
-  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
-  maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
-  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
-  warningMessage: "", // Warning message send to the user indicating they are going to fast.
-  banMessage: "", // Ban message, always tags the banned user in front of it.
-  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
-  maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
-  deleteMessagesAfterBanForPastDays: 7 // Delete the spammed messages after banning for the past x days.
-});
 
 
 
