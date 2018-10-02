0aaa2c30d628274feb85f7a2ddc703a27893ad80
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-02 19:56:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -321,35 +321,19 @@ const embed = new Discord.RichEmbed()
 
 
 
-
-
-
-const Discord = require ("discord.js");
+ const Discord = require('discord.js');
 const client = new Discord.Client();
-   
-client.on('message', function(message) {
-    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
-            let command = message.content.split(" ")[0];
-        if(message.content.includes('discord.gg')){
-        message.reply (' ')
-           if(!message.channel.guild) return message.reply('** This command only for servers**');
-     message.member.addRole(message.guild.roles.find('name', 'اسكت'));
-    const embed500 = new Discord.RichEmbed()
-      .setTitle(":x: | تمت معاقبتك")
-            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
-      .addField(`by`,`iMr.KBOOSH`)
-            .setColor("c91616")
-            .setThumbnail(`${message.author.avatarURL}`)
-            .setAuthor(message.author.username, message.author.avatarURL)
-        .setFooter(`${message.guild.name} Server`)
-     message.channel.send(embed500)
-   
-       
-    }
-    }
-})
- 
- 
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} !`);
+    client.user.setGame(`❦ العقل السليم في البعد عن الحريم `,`www.twitch.tv/AlphaCodes`);
+  });
+  
+  
+  client.login("NDc4OTU0ODAzNjU3OTY1NTY5.DpU70A.k5UlZRHoYmS-ak0_PO1uSEjaKmw");
+
+
 
 
 
