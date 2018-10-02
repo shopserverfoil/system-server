7b84e354555c16b6cdfe3cc6c804cc744173c352
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-02 21:38:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -356,90 +356,29 @@ const embed = new Discord.RichEmbed()
 
 
 
-
-
-
-
-
-client.on('message', async message =>{
-  var prefix = "+";  //alpha codes
-if (message.author.omar) return; //alpha codes
-if (!message.content.startsWith(prefix)) return; //alpha codes
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('MANAGE_ROLES'));
-if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.reply("**I Don't Have `MANAGE_ROLES` Permission**").then(msg => msg.delete(6000))
-var command = message.content.split(" ")[0];
-command = command.slice(prefix.length);
-var args = message.content.split(" ").slice(1);
-	if(command == "اسكت") {
-    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!tomute) return message.reply("**يجب عليك المنشن اولاّ**:x: ") .then(m => m.delete(5000));
-    if(tomute.hasPermission("MANAGE_MESSAGES"))return      message.channel.send('**للأسف لا أمتلك صلاحية** `MANAGE_MASSAGEES`');
-    let muterole = message.guild.roles.find(`name`, "muted");
-    //start of create role
-    if(!muterole){ //alpha codes
-      try{
-        muterole = await message.guild.createRole({
-          name: "muted",
-          color: "#000000",
-          permissions:[]
-        }) //alpha codes
-        message.guild.channels.forEach(async (channel, id) => {
-          await channel.overwritePermissions(muterole, {
-            SEND_MESSAGES: false,
-            ADD_REACTIONS: false
-          });
-        }); //alpha codes
-      }catch(e){ //alpha codes
-        console.log(e.stack);
-      } //alpha codes
+ client.on('message', message => {
+  if (message.content.startsWith(prefix + "فلوس")) {
+       if(!message.channel.guild) return;
+      var ment = message.mentions.members.first();
+      var getvalueof;
+      if(ment) {
+        getvalueof = ment;
+      } else {
+        getvalueof = message.author;
+      }
+      sql.get(`SELECT * FROM scores WHERE userId ="${getvalueof.id}"`).then(row => {
+        if (!row) return message.reply("sadly you do not have any points yet!");
+        message.channel.send(getvalueof.toString()+',** your :credit_card: balance is '+'`$'+`${row.points}`+'`**');
+        console.log('[credit] Send By: ' + message.author.username)
+      });
     }
-    //end of create role
-    let mutetime = args[1];
-    if(!mutetime) return message.reply("**يرجى تحديد وقت الميوت**:x:");
-  
-    await(tomute.addRole(muterole.id));
-    message.reply(`<@${tomute.id}> تم اعطائه ميوت ومدة الميوت : ${ms(ms(mutetime))}`);
-  
-    setTimeout(function(){
-      tomute.removeRole(muterole.id);
-      message.channel.send(`<@${tomute.id}> **انقضى الوقت وتم فك الميوت عن الشخص**:white_check_mark: `);
-    }, ms(mutetime));
-  
-  
-  //alpha codes
-  } //alpha codes
-});
-client.on('message', async message =>{
-  var prefix = "تكلم"; //alpha codes
-if (message.author.omar) return;
-if (!message.content.startsWith(prefix)) return;
-if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-if(!message.member.hasPermission('MANAGE_ROLES'));
-if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.reply("**I Don't Have `MANAGE_ROLES` Permission**").then(msg => msg.delete(6000))
-var command = message.content.split(" ")[0];
-command = command.slice(prefix.length);
-var args = message.content.split(" ").slice(1);
-if(command === `كلم`) {
-  if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.sendMessage("**ليس لديك صلاحية لفك عن الشخص ميوت**:x: ").then(msg => msg.delete(6000))
+    fs.writeFile("./daily.json", JSON.stringify(daily), (err) => {
+    if (err) console.error(err)
+  });
+  });
 
 
-  let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!toMute) return message.channel.sendMessage("**عليك المنشن أولاّ**:x: ");
 
-  let role = message.guild.roles.find (r => r.name === "muted");
-  
-  if(!role || !toMute.roles.has(role.id)) return message.channel.sendMessage("**لم يتم اعطاء هذه شخص ميوت من الأساس**:x:")
-
-  await toMute.removeRole(role)
-  message.channel.sendMessage("**لقد تم فك الميوت عن شخص بنجاح**:white_check_mark:");
-
-  return;
- //alpha codes
-  }
-
-}); //alpha codes
- //alpha codes
 
 
 
