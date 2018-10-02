c4c56cc261276fcf78acf38f5046800e385eb95d
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-02 20:32:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -413,10 +413,69 @@ module.exports.help = {
 }
 
 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 
 
+	 
+	 
+	 
+	 client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("483960386693890058");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+ 
+ 
+ 
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.get("483961152234061824");
+    if (!channel) {
+        console.log("وهمي");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('وهمي');
+    var guild;
+    while (!guild)
+        guild = client.guilds.get("483960386693890058");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;        
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+	 
+	 
+	 
+	 
+	 
 
 
+	 
+	 
+	 
+	 
 
 
 
