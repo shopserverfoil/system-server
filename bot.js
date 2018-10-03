d428c4addd2e697a885c35c201d6e617361d748f
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-03 20:34:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -360,6 +360,19 @@ const embed = new Discord.RichEmbed()
 
 
 
+client.on("message", (message) => {
+    if (message.content.startsWith("كيك")) {
+      if(!message.member.hasPermission('KICK_MEMBERS')) return message.reply('? ماعندك الصلاحيات');
+        var member= message.mentions.members.first();
+        member.kick().then((member) => {
+            message.channel.send(member.displayName + " مع السلامه :wave: ");
+        }).catch(() => {
+            message.channel.send("Error -_-");
+        });
+    }
+});
+
+
 
 
 
