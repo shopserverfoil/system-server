b9ba6867c310cdf43c13d3e63929685763ab75dc
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-03 21:53:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -361,38 +361,6 @@ const embed = new Discord.RichEmbed()
 
 
 
-client.on("message", (message) => {
-  var sender = message.author
-if(message.content.startsWith(prefix + 'كريديت')) {
-if (games[sender.id].lastDaily != moment().format('day')) {
-    games[sender.id].lastDaily = moment().format('day')
- games[message.author.id].credits += 200;
-    message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
-} else {
-    message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
-}
-}
-})
-client.on("message", (message) => {
-  if (message.author.bot) return;
-    if (message.author.id === client.user.id) return;
-    if(!message.channel.guild) return;      
-if (mes=age.content === '#credits') {
-message.channel.send(`** ${message.author.username}, your :credit_card: balance is ${games[message.author.id].credits}.**`)
-}
-});
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
