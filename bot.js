d8e1797cf1323c1078f11ef3b3fda30bd0964627
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-05 16:40:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -255,7 +255,7 @@ var embed = new Discord.RichEmbed()
 
  
 
-message.channel.send(`**<@${message.author.id}> لقد تم سحب العضو إليك ✅${message.guild.name}**`).then(msg => msg.delete(5000));
+message.channel.send(`**<@${message.author.id}> لقد تم سحب العضو إليك ✅${message.voiceChannel.name}**`).then(msg => msg.delete(5000));
 
  message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
 
