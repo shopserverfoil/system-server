0ff0052b66de6437732f7f9e0619fe480468d78b
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-05 03:32:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -349,68 +349,6 @@ const embed = new Discord.RichEmbed()
 
 
 
- 
-
-
-
-
-
-
-
-
-
-           
-
-client.on('ready', function(){    
-
-    var ms = 40000 ;    
-
-    var setGame = ['help'];    
-
-    var i = -1;    
-
-    var j = 0;    
-
-    setInterval(function (){    
-
-        if( i == -1 ){    
-
-j = 1;    
-
-       }    
-
-        if( i == (setGame.length)-1 ){    
-
-            j = -1;    
-
-      }    
-
-       i = i+j;    
-
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
-
-}, ms);    
-
-    
-
-});
-
-
-
-
-
-
-
-
-
-          
-
-
-
-
-            
-
-
 
 
 
