511179368f088ae0933932e8b2a6cdb9b9ba3aa4
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-05 04:03:18 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '!';
+
+client.on('ready', () => {
+
+    console.log(`Logged in as ${client.user.tag}`);
+
+});
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
