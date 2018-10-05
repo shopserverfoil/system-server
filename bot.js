dcc8116c5ad5298fdf0b0a8013fae533521fc2f8
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-06 02:19:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -277,104 +277,44 @@ client.on('ready', () => {
 
 
 
+  
+
+  client.on('message', message => {
+
+    if (message.content.startsWith("رابط")) {
+
+  message.channel.createInvite({
+
+        thing: true,
+
+        maxUses: 5,
+
+        maxAge: 86400
+
+    }).then(invite =>
+
+      message.author.sendMessage(invite.url)
+
+    )
+
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**").then(msg => msg.delete(3500));
+
+message.author.send(`**عدد استخدمات الرابط : 5 : مدة الرابط [يوم]**`)
 
 
-
- 
-
-client.on('message',async message => {
-
-   
-
-if(message.author.bot) return;
-
-if(!credits[message.author.id]) credits[message.author.id] = {
-
-    credits: 50
-
-};
-
- 
-
-let userData = credits[message.author.id];
-
-let m = userData.credits;
-
- 
-
-fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
-
-    if (err) console.error(err);
-
-  });
-
-  credits[message.author.id] = {
-
-      credits: m + 0.5,
-
-  }
-
- 
-
-    if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
-
-message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`);
-
-}
-
-});
-
- 
-
-client.on('message', async message => {
-
-    let amount = 250;
-
-    if(message.content.startsWith(prefix + "daily")) {
-
-    if(message.author.bot) return;
-
-    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
-
-   
-
-    let userData = credits[message.author.id];
-
-    let m = userData.credits + amount;
-
-    credits[message.author.id] = {
-
-    credits: m
-
-    };
-
- 
-
-    fs.writeFile("./creditsCode.json", JSON.stringify(userData.credits + amount), (err) => {
-
-    if (err) console.error(err);
-
-    });
-
-   
-
-    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
-
-        coolDown.add(message.author.id);
-
-    });
-
-   
-
-    setTimeout(() => {
-
-       coolDown.remove(message.author.id);
-
-    },86400000);
 
     }
 
 });
+
+  
+
+ 
+
+
+
+
+
 
 
 
