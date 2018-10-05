7bea8ed136582e696369d350da6874ab8f1809b8
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-05 14:27:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -163,7 +163,7 @@ client.on('message', message => {
 
             
 
-           message.channel.send let (`sendBC = msg.createReactionCollector(yesEmoji)`).then(msg => msg.delete(5000));
+            let sendBC = msg.createReactionCollector(yesEmoji);
 
             let dontSendBC = msg.createReactionCollector(noEmoji);
 
