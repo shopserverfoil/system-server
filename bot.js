5a59b5d8f6b385264584c4b0e6962ea395e60075
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-05 22:03:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -228,134 +228,6 @@ client.on('message', message => {
 
 
 
-
-
-
-client.on('message', async message => {
-  let args = message.content.split(" ");
-  if(message.content.startsWith(prefix + "اسكت")) {
-    if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply('# - ملحوظة :  يجب ان يكون لديك برمشن أداري . ').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
- 
-    if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.reply('# - ملحوظة : يجب ان يكون البوت لديه برمشن أداري').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
- 
-    let mention = message.mentions.members.first();
-    if(!mention) return message.reply('# - ملحوظة : يجب ان تقوم بمنشن شخص معين .').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
- 
-    if(mention.highestRole.position >= message.guild.member(message.author).highestRole.positon) return message.reply('# - ملحوظة : لا يمكنك اعطاء ميوت لشخص اعلي من رتبتك .').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
-    if(mention.highestRole.positon >= message.guild.member(client.user).highestRole.positon) return message.reply('# - ملحوظه : لا يمكنك اعطاء ميوت لشخص اعلي من رتبتك').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
-    if(mention.id === message.author.id) return message.reply('# - ملحوظه : لا يمكنك ان تعطي ميوت لنفسك .').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
- 
-    let duration = args[2];
-    if(!duration) return message.reply('# - ملحوظه : يجب ان تضع وقت .').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
- 
-    if(isNaN(duration)) return message.reply('# - ملحوظه : يجب تحديد وقت زمني صحيح').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
- 
-    let sbb = message.content.split(" ").slice(3).join(" ");
-    if(!sbb) sbb = "غير معروف .";
- 
-    let thisEmbed = new Discord.RichEmbed()
-    .setAuthor(mention.user.username, mention.user.avatarURL)
-    .setTitle('# - لقد تم أعطائك ميوت .')
-    .setThumbnail(mention.user.avatarURL)
-    .addField('# - السيرفر',message.guild.name,true)
-    .addField('# - تم اعطائك ميوت بواسطة',message.author,true)
-    .addField('# - السبب',reason)
- 
-    let role = message.guild.roles.find('name', 'Muted') || message.guild.roles.get(r => r.name === 'Muted');
-    if(!role) try {
-      message.guild.createRole({
-        name: "Muted",
-        permissions: 0
-      }).then(r => {
-        message.guild.channels.forEach(c => {
-          c.overwritePermissions(r , {
-            SEND_MESSAGES: false,
-            READ_MESSAGES_HISTORY: false,
-            ADD_REACTIONS: false
-          });
-        });
-      });
-    } catch(e) {
-      console.log(e.stack);
-    }
-    mention.addRole(role).then(() => {
-      mention.send(thisEmbed);
-      message.channel.send(`**:white_check_mark: ${mention.user.username} Muted ! :zipper_mouth:  **  `);
-      mention.setMute(true);
-    });
-    setTimeout(() => {
-      if(duration === 0) return;
-      if(!mention.has.roles(role)) return;
-      mention.setMute(false);
-      mention.removeRole(role);
-      message.channel.send(`**:white_check_mark: ${mention.user.username} Unmuted **   `);
-    },duration * 60000);
-  } else if(message.content.startsWith(prefix + "unmute")) {
-    let mention = message.mentions.members.first();
-    let role = message.guild.roles.find('name', 'Muted') || message.guild.roles.get(r => r.name === 'Muted');
-    if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply('# - ملحوظة :  يجب ان يكون لديك برمشن أداري . ').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
- 
-    if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.reply('# - ملحوظة : يجب ان يكون البوت لديه برمشن أداري').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
- 
-    if(!mention) return message.reply('# - ملحوظه : يجب منشن شخص لفك الميوت عنهه .').then(msg => {
-      msg.delete(3500);
-      message.delete(3500);
-    });
- 
-      mention.removeRole(role);
-      mention.setMute(false);
-      message.channel.send(`**:white_check_mark: ${mention.user.username} Unmuted ! **  `);
-  }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 client.on('message', message => {
 
  if(message.content.startsWith(prefix + "تعال")) {
@@ -389,6 +261,124 @@ client.on('ready', () => {
     client.channels.get("490147934919458817").join();
 
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
+
+const coolDown = new Set();
+
+ 
+
+client.on('message',async message => {
+
+   
+
+if(message.author.bot) return;
+
+if(!credits[message.author.id]) credits[message.author.id] = {
+
+    credits: 50
+
+};
+
+ 
+
+let userData = credits[message.author.id];
+
+let m = userData.credits;
+
+ 
+
+fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
+
+    if (err) console.error(err);
+
+  });
+
+  credits[message.author.id] = {
+
+      credits: m + 0.5,
+
+  }
+
+ 
+
+    if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
+
+message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`);
+
+}
+
+});
+
+ 
+
+client.on('message', async message => {
+
+    let amount = 250;
+
+    if(message.content.startsWith(prefix + "daily")) {
+
+    if(message.author.bot) return;
+
+    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
+
+   
+
+    let userData = credits[message.author.id];
+
+    let m = userData.credits + amount;
+
+    credits[message.author.id] = {
+
+    credits: m
+
+    };
+
+ 
+
+    fs.writeFile("./creditsCode.json", JSON.stringify(userData.credits + amount), (err) => {
+
+    if (err) console.error(err);
+
+    });
+
+   
+
+    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
+
+        coolDown.add(message.author.id);
+
+    });
+
+   
+
+    setTimeout(() => {
+
+       coolDown.remove(message.author.id);
+
+    },86400000);
+
+    }
+
+});
+
 
 
 
