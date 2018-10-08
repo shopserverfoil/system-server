8bb6630a3215e4aec24ef94f1b92ab16128dc25c
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-08 04:17:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -414,7 +414,7 @@ client.on('message', async message => {
     });
 
     let mention = message.mentions.members.first();
-    if(!mention) return message.reply('**منشن عضو لأسكاته ( لأعطائة ميوت ) كتابي**').then(msg => {
+    message.channel.send('**منشن عضو لأسكاته ( لأعطائة ميوت ) كتابي**').then(msg => {
       msg.delete(3500);
       message.delete(3500);
     });
