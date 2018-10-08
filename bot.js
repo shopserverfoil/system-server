6938fd3a2195473143d29a8487009de3bb35e398
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-08 23:53:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -548,14 +548,23 @@ client.on('message', async message => {
 
 
 client.on('message',async message => {
+
     if(message.content === 'اسكت') {
+
       if(!message.member.hasPermission('MUTE_MEMBERS')) return      message.channel.send('' );
-     var embed = new Discord.RichEmbed() 
-        .setColor('')
+
+   var embed = new Discord.RichEmbed() 
+
+        
+
         .setImage('https://cdn.discordapp.com/attachments/495582423430463498/498283222011084810/n4eSIakPew.png').then(msg => {msg.delete(5000)};
-        message.channel.sendEmbed(embed);
+
+        
+
     
+
     }
+
      });
 
 
