5f14afad4bf2b5504bd00abd9277b6cd8a3be5b2
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-10 01:39:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -565,7 +565,7 @@ client.on('message', async message => {
 client.on('message', message => {
 
             if (message.content === 'السلام عليكم') {
-  message.channel.send('**وعليكم السلام منور 🌹**');
+  message.channel.send('**وعليكم السلام منور 🌹**').then(msg => msg.delete(5000);
 
 }
 
@@ -633,7 +633,7 @@ if(Del.content === 'تكلم') {
   Del.channel.send('**:information_source:  ``#unmute @Dramex`` يجب تحديد شخص**')
 
       
-Del.channel.sendEmbed(embed).then(message => {message.delete(7000)})
+
 
 }
 
