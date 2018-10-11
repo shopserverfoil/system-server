269a13396e647ef94dc5c383cb30c5c2a2ea8446
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-11 05:39:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -664,24 +664,6 @@ message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 client.on('message',async message => {
 
 if(message.content === 'بان') {
@@ -703,6 +685,125 @@ message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
 }
 
  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', async message => {
+    let args = message.content.split(" ");
+    let command = args[0];
+
+    if(command === prefix + 'باند') {
+      if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send('انت لا تملك الصلاحيات اللازمة').then(msg => {
+        msg.delete(3500);
+        message.delete(3500);
+      });
+
+      if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.channel.send('انا لا املك الصلاحيات اللازمة. يحب توفر صلاحيات `Ban Members , Embed Links`').then(msg => {
+        msg.delete(3500);
+        message.delete(3500);
+      });
+
+      let mention = message.mentions.members.first();
+      if(!mention) return message.channel.send('').then(msg => {
+        msg.delete(3500);
+        message.delete(3500);
+      });
+        
+     
+         if(mention.hasPermission('BAN_MEMBERS')) return message.channel.send(`**لا يمكنك اعطاء باند لآحد آدارة السيرفر ❌**`);
+        
+        
+        if(mention.position >= message.guild.member(message.author).positon) return message.channel.send('').then(msg => {
+      msg.delete(3500);
+      message.delete(3500);
+    });
+        
+        
+      if(mention.positon >= message.guild.member(client.user).positon) return message.channel.send('').then(msg => {
+      msg.delete(3500);
+      message.delete(3500);
+    });
+        
+        
+       if(mention.id === message.author.id) return message.channel.send('').then(msg => {
+      msg.delete(3500);
+      message.delete(3500)
+      });
+
+       let duration = args[2];
+    if(!duration)  message.channel.send('').then(msg => {
+      msg.delete(3500);
+      message.delete(3500);
+    });
+        
+       if(isNaN(duration))  message.channel.send('').then(msg => {
+      msg.delete(3500);
+      message.delete(3500);
+    });
+
+       let reason = message.content.split(" ").slice(3).join(" ");
+    if(!reason) reason = " [ ** __لاتسب | بدون سبام__** ] ";
+ 
+    let thisEmbed = new Discord.RichEmbed()
+    .setAuthor(mention.user.username, mention.user.avatarURL)
+    .setTitle('**تم آعطائك باند**')
+    .addField('**__السيرفر__**',[ message.guild.name ])
+    .addField('**__تم آعطائك باند بواسطة__**', [ message.author ])
+    .addField('**__آلسبب__**',reason)
+       mention.send(thisEmbed).then(() => {
+       mention.ban({
+         reason: reason,
+       });
+       message.channel.send(`**:white_check_mark: ${mention.user.username} banned from the server ! :airplane: **  `)
+       setTimeout(() => {
+         if(duration === 0) return;
+         message.guild.unban(mention);
+       },duration * 60000);
+     });
+   }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
