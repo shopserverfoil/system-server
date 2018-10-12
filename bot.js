f14170f8e39652f97e2a48b111c79f7b16181b81
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-12 21:28:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -829,68 +829,6 @@ client.on('message', async message => {
 
 
 
-client.on('message', message => {
- 
-    var prefix = "";
- 
-if(!message.channel.guild) return;
- 
-if(message.content.startsWith(prefix + 'السحب')) {
- 
- if (message.member.hasPermission("MOVE_MEMBERS")) {
- 
- if (message.mentions.users.size === 0) {
- 
- return message.channel.send("" +prefix+ "** ❌  لم يتم العثور على العضو المطلوب **").then(msg => msg.delete(5000));
- 
-}
- 
-if (message.member.voiceChannel != null) {
- 
- if (message.mentions.members.first().voiceChannel != null) {
- 
- var authorchannel = message.member.voiceChannelID;
- 
- var usermentioned = message.mentions.members.first().id;
- 
-var embed = new Discord.RichEmbed()
- 
- .setTitle("Succes!")
- 
- 
- 
- 
- 
-var embed = new Discord.RichEmbed()
- 
-  let mentions = message.mentions.members.first();
- 
-message.channel.send(`**✅ @${mentions.user.username} moved to ${`message.member.voiceChannel.name`}**`).then(msg => msg.delete(5000));
- 
- message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
- 
-message.guild.members.get(usermentioned).send(embed)
- 
-} else {
- 
-message.channel.send("** ❌  العضو يجب أن يكون متواجد بروم صوتي **").then(msg => msg.delete(5000));
- 
-}
- 
-} else {
- 
- message.channel.send("** ❌  You must be in voice channel !**").then(msg => msg.delete(5000));
- 
-}
- 
-} else {
- 
-message.react("")
- 
- }}});
-
-
-
 
 
 
