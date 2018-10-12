4c755eb28c7905ee63f258f857aa38e6f7be4315
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-12 17:00:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -352,7 +352,7 @@ var embed = new Discord.RichEmbed()
 
   let mention = message.mentions.members.first();
 
-message.channel.send(`**:white_check_mark: ${mention.user.username} moved to ${message.member.voiceChannel.name}**`).then(msg => msg.delete(5000));
+message.channel.send(`**:white_check_mark: ${mention.user.tag} moved to ${message.member.voiceChannel.name}**`).then(msg => msg.delete(5000));
 
  message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
 
