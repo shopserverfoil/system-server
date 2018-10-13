5c2fdc7566be6b6eb8b7bbbb5c46efaeaaf6ed86
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-13 02:40:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -863,7 +863,7 @@ client.on("guildMemberAdd", (member) => {
                 if (dat[Inv] < Invite.uses) {
                     console.log(3);
                     console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
- channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
+ channel.send(`** Invited by :  ${Invite.inviter}  **`)            
  }
             dat[Inv] = Invite.uses;
         })
