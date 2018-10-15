6c8697508ab1071e5806d5538893a345f95b65ac
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-15 04:47:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -260,7 +260,7 @@ client.on('message', msg => {
 
 client.on('ready', () => {
 
-    client.channels.get("501109014780706826").join();
+    client.channels.get("501188858755678233").join();
 
     });
 
@@ -983,6 +983,146 @@ client.on("message", async msg => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("message", async message => {
+
+  if(message.author.bot) return;
+
+  if(message.channel.type === "dm") return;
+
+  let prefix = "كيك";
+
+  let messageArray = message.content.split (" ");
+
+  let cmd = messageArray[0];
+
+  let args = messageArray.slice(1);
+
+    if(cmd === `${prefix}kick`){
+
+      let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+      if(!kUser) return message.channel.send("فين العضو ؟");
+
+      let kReason = args.join(" ").slice(22);
+
+      if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("ما عندك برمشن");
+
+      if(kUser.hasPermission("MANAGE_CHANNELS")) return message.channel.send("ما تقدر تسوي كيك للأدمين")
+
+      let kickEmbed = new Discord.RichEmbed()
+
+      .setDescription("~Kick~")
+
+      .setColor("#e56b00")
+
+      .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+
+      .addField("Kicked By", `<@${message.author.id}> with the id ${message.author.id}`)
+
+      .addField("Kicked In", message.channel)
+
+      .addField("Time", message.createdAt)
+
+      .addField("Reason", kReason);
+
+      let kickChannel = message.guild.channels.find('name', 'kick-ban');
+
+      if(!kickChannel) return message.channel.send("لم اجد روم ال kick-ban");
+
+      message.guild.member(kUser).kick(kReason)
+
+      kickChannel.send(kickEmbed);
+
+    }
+
+    });
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const client = new d.Client();
+var prefix ="!"//:3 غيره بكيفك
+var colors = new Array(100);
+
+client.on("message", (message) => {
+    var command = message.content.split(" ")[0].slice(prefix.length);
+    switch(command) {
+        case "create-colors" :
+        if (!message.channel.type == "text") return;
+        if (!message.member.hasPermission("MANAGE_ROLES")) return
+        for(var x = 0;x<colors.length;x++){
+            message.guild.createRole({name : x, color : colors[x]});
+        };
+    };
+})
+function sin_to_hex(i, phase) {
+    var sin = Math.sin(Math.PI / size * 2 * i + phase);
+    var int = Math.floor(sin * 127) + 128;
+    var hex = int.toString(16);
+  
+    return hex.length === 1 ? '0'+hex : hex;
+};
+for(var x=0;x<colors.length;x++) {
+    let r   = sin_to_hex(i, 0 * Math.PI * 2/3);
+    let b  = sin_to_hex(i, 1 * Math.PI * 2/3);
+    let g = sin_to_hex(i, 2 * Math.PI * 2/3);
+    colors[x] = '#'+ r + g + b;
+};
 
 
 
