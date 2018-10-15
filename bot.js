685eb38de61f90b9e73f371fdf9ae9db1de2676c
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-15 11:38:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -972,8 +972,8 @@ client.on("guildMemberAdd", (member) => {
       let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
       if(!kUser) return message.channel.send("فين العضو ؟");
       let kReason = args.join(" ").slice(22);
-      if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("ما عندك برمشن");
-      if(kUser.hasPermission("MANAGE_CHANNELS")) return message.channel.send("ما تقدر تسوي كيك للأدمين")
+      if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("ما عندك برمشن");
+      if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("ما تقدر تسوي كيك للأدمين")
 
       let kickEmbed = new Discord.RichEmbed()
       .setDescription("~Kick~")
