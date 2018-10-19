1e314626d4980cb18cf3a5603e883be970cb2817
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-19 18:42:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1176,30 +1176,44 @@ setTimeout(() => {
 
 
 
-client.on('message', message => {
+
+
+
+  client.on('message', message => {
+
     if (message.content.startsWith(prefix + 'مسح')) {
+
       if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(``).catch(console.error);
+
   message.delete()
+
   if(!message.channel.guild) return;
+
   let args = message.content.split(" ").slice(1);
+
   
+
   const messagecount = parseInt(args.join(' '));
+
   
+
   message.channel.fetchMessages({
+
   
+
   limit: messagecount
+
   
+
   }).then(messages => message.channel.bulkDelete(messages));
-  message.channel.sendMessage(`\`\`\`عدد الرسائل التي تم مسحها: ${args}\`\`\``)
 
-}}).then(msg => {msg.delete(3000)});
+  message.channel.sendMessage(`\`\`\`عدد الرسائل التي تم مسحها: ${args}\`\`\``).then(msg => {msg.delete(3000)});
+
   };
+
   
+
   });
-
-
-
-  
 
 
 
