7e2e2d08007f203c8e4a5d307964824d7821d734
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-21 14:17:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -230,39 +230,7 @@ client.on('message', message => {
 
 
 
-client.on('message', message => {
 
- if(message.content.startsWith(prefix + "تعال")) {
-
-message.member.voiceChannel.join();
-
-}
-
-});
-
-client.on('message', msg => {
-
-    if (msg.content == '1join') {
-
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-
-         msg.member.voiceChannel.join().then(msg.react('✅'));
-
-     }
-
-    }
-
-}
-
-})
-
-client.on('ready', () => {
-
-    client.channels.get("501413605854674966").join();
-
-    });
 
 
 
