35ae401e31a9d1cac365388153d91084516f27e9
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-25 01:20:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1279,7 +1279,66 @@ client.on('message', msg => {
 
 
 
+client.on('guildMemberAdd', member => {
+     const welcomer =  member.guild.channels.find('name', 'pharaohs');
 
+const w = ['./w1.png'];
+         let Image = Canvas.Image,
+            canvas = new Canvas(400, 239),
+            ctx = canvas.getContext('2d');
+        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+            if (err) return console.log(err);
+            let BG = Canvas.Image;
+            let ground = new Image;
+            ground.src = Background;
+            ctx.drawImage(ground, 0, 0, 400, 239);
+             
+          
+
+                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(100) + ".png" : member.user.displayAvatarURL;
+                jimp.read(url, (err, ava) => {
+                    if (err) return console.log(err);
+                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                        if (err) return console.log(err);
+
+                        ctx.font = "bold 12px Arial";
+                        ctx.fontSize = '20px';
+                        ctx.fillStyle = "#f1f1f1";
+                        ctx.textAlign = "center";
+                        ctx.fillText(``, 300, 130);
+
+                        ctx.font = "bold 26px Arial";
+                        ctx.fontSize = '21px';
+                        ctx.fillStyle = "#f1f1f1";
+                        ctx.textAlign = "center";
+                        ctx.fillText(member.user.username, 240, 210);
+ 
+                let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.beginPath();
+                              ctx.arc(84, 149, 72, 0, Math.PI*2);
+                              ctx.stroke();
+                                 ctx.clip();
+                                 ctx.drawImage(ava, 12, 77, 149, 149);
+                          
+                
+                             
+welcomer.sendFile(canvas.toBuffer())
+
+
+
+      
+      
+                    }  )  
+      
+                    
+
+})
+      });     
+            
+ 
+});
 
 
 
