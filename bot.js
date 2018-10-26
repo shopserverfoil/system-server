e80d42862777869ab52f4229b328718883dc3241
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-10-27 01:07:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1306,21 +1306,6 @@ client.on('message', msg => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
 client.on("message", message => {
   if (message.author.bot) return;
@@ -1425,6 +1410,23 @@ mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${arg
 }
  
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+
 
 
 
